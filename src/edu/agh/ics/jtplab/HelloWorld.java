107b35bdc33c105496bc3df303c61de80d2d5269
konradmikucki <komik966@gmail.com>
2017-10-20 22:54:20 +0200

    Arguments for loop

--- src/edu/agh/ics/jtplab/HelloWorld.java
+++ src/edu/agh/ics/jtplab/HelloWorld.java
@@ -2,6 +2,8 @@ package edu.agh.ics.jtplab;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
     }
 }
